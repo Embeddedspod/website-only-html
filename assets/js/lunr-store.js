818d8b46bb3da277b63185e00506c5f52e2ba224
Embeddedspod <76859816+Embeddedspod@users.noreply.github.com>
2021-01-12 20:29:15 +0530

    Add files via upload

---
+++ assets/js/lunr-store.js
@@ -0,0 +1,27 @@
+var store = [ 
+     
+    { 
+        "url": "/embeddedpod-website-2/404.html",
+        "title": "404?",
+        "text": "Sorry, but the page you were trying to view does not exist. ____________ ------------ \\ ^__^ \\ (oo)\\_______ (__)\\ )\\/\\ ||----w | || ||"
+    }, 
+    { 
+        "url": "/embeddedpod-website-2/about.html",
+        "title": "About This Template",
+        "text": "[bootstrap-template](https://github.com/thecdil/bootstrap-template) is a basic template repository to create a [Bootstrap](https://getbootstrap.com/) site using Jekyll on GitHub Pages (or where every you want to host it). The layout is based on the [Bootstrap starter template example](https://getbootstrap.com/docs/4.5/examples/) with a navbar, search box (using lunr.js), and sticky footer. It is intended as a quick starting point for creating new web projects. The basic steps for using bootstrap-template are: 1. create repository from the template 2. edit _config.yml 3. edit content pages 4. use customization options See [docs/create-website.md](https://github.com/thecdil/bootstrap-template/blob/main/docs/create-website.md) for details!"
+    }, 
+    { 
+        "url": "/embeddedpod-website-2/example.html",
+        "title": "Example Page",
+        "text": "Write pages in Markdown. ## Heading Two Any text with no empty lines between will become a paragraph. Leave an blank line between headings and paragraphs. Font can be *Italic* or **Bold**. Code can be highlighted with `backticks`. Hyperlinks look like this [GitHub Help](https://help.github.com/). ### Lists A bullet list is created using `*`, `+`, or `-`, like: - dog - cat - muffin A numbered list is created using a number + `.`, like: 1. one 2. two 6. three 2. four Horizontal rule: -------------- {% include figure.html img=\"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/VAN_CAT.png/480px-VAN_CAT.png\" alt=\"white cat\" caption=\"I found this [Cat image on Wikimedia](https://commons.wikimedia.org/wiki/File:VAN_CAT.png).\" %} > A block quote. > Is like this. {:.blockquote} ### A Table | header | column a | column b | | --- | --- | --- | | dogs | 3 | 6 | | cats | 3 | 6 | | muffins | 15 | 30 | {:.table .table-striped}"
+    }, 
+    { 
+        "url": "/embeddedpod-website-2/index.html",
+        "title": "Home",
+        "text": "{% include jumbotron.html title=site.title lead=\"Create a site from a basic bootstrap template\" btn-text=\"Learn More\" btn-link=\"about.html\" %}"
+    }, 
+    { 
+        "url": "/embeddedpod-website-2/search.html",
+        "title": "Site Search",
+        "text": "Search site content using Lunr.js."
+    }];
